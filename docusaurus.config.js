2dee7dba5796f8989074e8f895f3fd425a1ec93f
Johannes Liermann <liermann@uni-mainz.de>
2021-08-27 17:04:36 +0200

    fix: github urls

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -86,7 +86,7 @@ module.exports = {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/NFDI4Chem/knowledge_base',
+            'https://github.com/NFDI4Chem/knowledge_base/tree/main/',
        
         },
         theme: {
