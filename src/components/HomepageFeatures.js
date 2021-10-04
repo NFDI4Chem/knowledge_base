b7bcd49961af1e8820e457d8901e64698af85fb3
Johannes Liermann <liermann@uni-mainz.de>
2021-10-04 18:13:40 +0200

    chore: new design without logo

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -6,41 +6,42 @@ import Link from '@docusaurus/Link';
 const FeatureList = [
   {
     title: 'Roles',
-    Svg: require('../../static/img/role.svg').default,
+    Svg: require('../../static/img/role_white.svg').default,
     link: '/docs/role/guide',
   },
   {
     title: 'Domains',
-    Svg: require('../../static/img/domain.svg').default,
+    Svg: require('../../static/img/domain_white.svg').default,
     link: '/docs/domains/guide',
   },
   {
     title: 'Problems',
-    Svg: require('../../static/img/problem.svg').default,
+    Svg: require('../../static/img/problem_white.svg').default,
     link: '/docs/problems/dmp',
   },
   {
     title: 'Topics & Concepts',
-    Svg: require('../../static/img/topics.svg').default,
+    Svg: require('../../static/img/topics_white.svg').default,
     link: '/docs/topics/fair',
   },
 ];
 
-function Feature({ Svg, title, link, description }) {
+function Feature({ Svg, title, link }) {
   return (
-    <div className={clsx('col col--3')} style={{
+    <div className={clsx('col')} style={{
+      width: 250,
       padding: 5,
       //backgroundColor: 'red',
     }}>
-    <div className='text--center'>
-        <Link to={link}><Svg className={styles.featureSvg} alt={title} /></Link>
-      </div>
       <div className='text--center'>
         {link ? (
           <Link
-            className={clsx('button button--secondary button--lg')}
+            className={clsx('button button--secondary button--lg')} style={{
+              width: 250,
+            }}
             // ? <Link className={clsx('')}
             to={link}>
+              <Svg className={styles.featureSvg} alt={title} /><br/>
             {title}
           </Link>
         ) : (
@@ -59,6 +60,30 @@ export default function HomepageFeatures() {
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
           ))}
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export function RoleButton() {
+  return (
+    <section className={styles.features}>
+      <div className='container'>
+        <div className='row--3'>
+          <Feature {...FeatureList[0]} />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export function DomainButton() {
+  return (
+    <section className={styles.features}>
+      <div className='container'>
+        <div className='row--3'>
+          <Feature {...FeatureList[1]} />
         </div>
       </div>
     </section>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,17 +5,22 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <h1 className="hero__title text">
+          {siteConfig.title}
+        </h1>
+        <p className="hero__subtitle">
+            {siteConfig.tagline}
+        </p>
         <div className={styles.buttons}>
           <Link
-            className="button button--primary button--lg"
+            className={clsx("button button--negative button--lg")}
             to="/docs/intro">
             Get started
           </Link>
