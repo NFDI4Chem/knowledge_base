1e2e7a79bf8bf4ed88a780214fbaefb407bf70dc
Johannes Liermann <liermann@uni-mainz.de>
2021-10-20 16:11:08 +0200

    fix: index.js alignment improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,19 +12,25 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero', styles.heroBanner)}>
       <div className="container">
-        <h1 className="hero__title text">
-          <img className="n4c--logo" src={useBaseUrl("/img/NFDI4Chem-Logo_einfarbig_weiss_nurGraphik.svg")} width="120" />
-          {siteConfig.title}
-        </h1>
-        <p className="hero__subtitle">
-            {siteConfig.tagline}
-        </p>
-        <div className={styles.buttons}>
-          <Link
-            className={clsx("button button--negative button--lg")}
-            to="/docs/intro">
-            Get started
-          </Link>
+        <div className="row">
+          <div className="col col--2 n4c--logo">
+            <img src={useBaseUrl("/img/NFDI4Chem-Logo_einfarbig_weiss_nurGraphik.svg")} width="120" />
+          </div>
+          <div className="col col--8">
+            <h1 className="hero__title text">
+              {siteConfig.title}
+            </h1>
+            <p className="hero__subtitle">
+              {siteConfig.tagline}
+            </p>
+            <div className={styles.buttons}>
+              <Link
+                className={clsx("button button--negative button--lg")}
+                to="/docs/intro">
+                Get started
+              </Link>
+            </div>
+          </div> 
         </div>
       </div>
     </header>
