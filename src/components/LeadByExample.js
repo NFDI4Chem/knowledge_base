15df490bdfaf50c205e79f9e187f7348e4338e38
Johannes Liermann <liermann@uni-mainz.de>
2022-10-17 16:26:36 +0200

    chore: lbe hovering cards

--- src/components/LeadByExample.js
+++ src/components/LeadByExample.js
@@ -12,46 +12,46 @@ export function MultiUrl( {name,url} ) {
 
 export function Lbeblock( {title, authors, link_pub, link_data, link_comment, description, tags} ) {
     return (
-        <div className="block_lbe">
+      <div className="col col--4"><div className="block_lbe">
 
-          <h3>{title}</h3>
+        <h3>{title}</h3>
 
-          <p>{tags.map((tag) => 
-            <button className="lbe_tag">
-              {tag}
-            </button>
-          )}</p>
+        <p>{tags.map((tag) => 
+          <button className="lbe_tag">
+            {tag}
+          </button>
+        )}</p>
 
-          <details className="details_lbe">
+        <details className="details_lbe">
 
-            <summary>Details</summary>
+          <summary>Details</summary>
 
-            <h4>Authors</h4>
-            
-            <p><em>{authors}</em></p>
-
-            <h4>Description</h4>
-
-            <p>{description}</p>
-
-            <h4>Links</h4>
-
-            <ul>
-              <li><a href={link_pub}>Link to publication</a></li>
-              <li>Link(s) to datasets:<br />{
-                link_data.map((props, idx) => (
-                  <MultiUrl key={idx} {...props} />
-                ))} <br /><em>{link_comment}</em></li>
-            </ul>        
-          </details>
+          <h4>Authors</h4>
           
-        </div>
+          <p><em>{authors}</em></p>
+
+          <h4>Description</h4>
+
+          <p>{description}</p>
+
+          <h4>Links</h4>
+
+          <ul>
+            <li><a href={link_pub}>Link to publication</a></li>
+            <li>Link(s) to datasets:<br />{
+              link_data.map((props, idx) => (
+                <MultiUrl key={idx} {...props} />
+              ))} <br /><em>{link_comment}</em></li>
+          </ul>        
+        </details>
+      </div></div>
+
   );
 }
 
 export default function Lbe() {
   return (
-      <div>
+      <div className="row">
         {lbeTable.map((props, idx) => (
           <Lbeblock key={idx} {...props} />
         ))}
