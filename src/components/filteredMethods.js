c55830c8947f344bcfa83fca7abf83b576d4165d
Johannes Liermann <liermann@uni-mainz.de>
2022-10-20 16:52:27 +0200

    chore: implemented basic filter

--- src/components/filteredMethods.js
+++ src/components/filteredMethods.js
@@ -1,28 +1,35 @@
-import React from 'react';
+import React,{ useState } from 'react';
 import MethodsProfileTable from '@site/src/components/MethodsProfile.js';
 
 var profiles = require('@site/static/assets/profiles.json');
 
 export default function FilteredProfile( {children, props } ) {
+
+    const [filterProfile, setFilterProfile] = useState("all");
+
+    function FilterButton( { name, longname } ) {
+        console.log("11: name:"+name+", longname:"+longname);
+    
+        return (
+            <button 
+                onClick={() => setFilterProfile(name)} 
+            >
+                {longname}
+            </button>
+        )
+    }
+    
     var result = [];
 
     result = profiles.filter(m => props.includes(m.name));
+    console.log(result,props);
+
+    console.log("32:");
+    console.log(filterProfile);
 
     return (
-        <div>{result.map((i,j) => <FButton short={j.name} long={j.longname} />)}</div>
-    )
+        <><div>{result.map((props,idx) => <FilterButton key={idx} {...props} />)}</div>
+        <div><MethodsProfileTable profile_to_show={filterProfile} /></div></>    
+)
 }
 
-function FButton( { children, short, long } ) {
-    return (
-        <button onClick={execFilter({short})} >{long}</button>
-    )
-}
-
-function execFilter( {props} ) {
-    return (
-        <div>{props}</div>
-    )
-}
-
-/*  */
