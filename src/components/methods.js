03ed1b6246f6d21ed4e5211e2ad581bfad5e905f
Johannes Liermann <liermann@uni-mainz.de>
2022-10-21 17:14:07 +0200

    chore: clean-up methods table

--- src/components/methods.js
+++ src/components/methods.js
@@ -1,6 +1,52 @@
-import React from 'react';
+import React,{ useState } from 'react';
 
 var table = require('@site/static/assets/methods.json');
+
+var table = require('@site/static/assets/methods.json');
+var profiles = require('@site/static/assets/profiles.json');
+
+export default function Methods( {defaultProfile} ) {
+
+    const [filterProfile, setFilterProfile] = useState(defaultProfile);
+    const [searchFilter, setSearchFilter] = useState("");
+    const handleChange = e => {setSearchFilter(e.target.value); setFilterProfile("")};
+
+    function FilterButton( { name, longname } ) {
+
+        var buttonClass = "lbe_tag";
+
+        if (name == filterProfile) {
+            buttonClass = "lbe_tag_active";
+        }
+        
+        return (
+            <button 
+                className={buttonClass}
+                onClick={() => setFilterProfile(name)} 
+            >
+                {longname}
+            </button>
+        )
+    }
+
+    var result = [];
+    var resultSet = [];
+
+    if (searchFilter == "") {
+        result = profiles.filter(m => m.name.includes(filterProfile));
+        resultSet = result.map(n => n.methods)[0];
+    }
+    else {
+        result = table.filter(obj => Object.keys(obj).some(key => obj[key].toLowerCase().includes(searchFilter.toLowerCase())));
+        resultSet = result.map(m => m.shortname);
+    }
+
+    return (
+        <><div className="block_filter">Click to filter: {profiles.map((props,idx) => <FilterButton key={idx} {...props} />)}<br />
+        Type to search: <input value={searchFilter} onChange={handleChange} /></div>
+        <div><MethodsTable methods_to_show={resultSet} /></div></>    
+    )
+}
 
 function Entry({ analytical_method, exemplary_proprietary_file_extensions, typical_size_of_proprietary_file, converter_to_open_file_format, recommendation_for_open_file_extension, file_format, file_size_of_open_format }) {
     return (
@@ -8,7 +54,7 @@ function Entry({ analytical_method, exemplary_proprietary_file_extensions, typic
     )
 }
 
-export default function MethodsTable({children, methods_to_show}) {
+function MethodsTable({methods_to_show}) {
 
     if(methods_to_show[0]==="all"){
         return (
@@ -22,8 +68,7 @@ export default function MethodsTable({children, methods_to_show}) {
         );
     }
 
-    let found = [];
-    found = table.filter(m => methods_to_show.includes(m.analytical_method) || methods_to_show.includes(m.shortname));
+    var found = table.filter(m => methods_to_show.includes(m.shortname));
 
     return (
     <table><thead><tr><th align="left">Analytical method</th><th align="left">Exemplary proprietary file extensions</th><th align="left">Typical size of proprietary file</th><th align="left">Converter to open file format</th><th align="left">Recommendation for open file extension*</th><th align="left">File format</th><th align="left">File size of open format</th></tr></thead><tbody>
--- src/components/searchMethods.js
+++ src/components/searchMethods.js
@@ -2,24 +2,47 @@ import React,{ useState } from 'react';
 import MethodsTable from '@site/src/components/methods.js'
 
 var table = require('@site/static/assets/methods.json');
+var profiles = require('@site/static/assets/profiles.json');
 
 export default function SearchMethods() {
 
+    const [filterProfile, setFilterProfile] = useState("all");
     const [searchFilter, setSearchFilter] = useState("");
-    const handleChange = e => {setSearchFilter(e.target.value)};
+    const handleChange = e => {setSearchFilter(e.target.value); setFilterProfile("")};
 
-    
+    function FilterButton( { name, longname } ) {
+
+        var buttonClass = "lbe_tag";
+
+        if (name == filterProfile) {
+            buttonClass = "lbe_tag_active";
+        }
+        
+        return (
+            <button 
+                className={buttonClass}
+                onClick={() => setFilterProfile(name)} 
+            >
+                {longname}
+            </button>
+        )
+    }
+
+    var result = [];
     var resultSet = [];
+
     if (searchFilter == "") {
-        resultSet = ["all"];
+        result = profiles.filter(m => m.name.includes(filterProfile));
+        resultSet = result.map(n => n.methods)[0];
     }
     else {
-        var result = table.filter(obj => Object.keys(obj).some(key => obj[key].toLowerCase().includes(searchFilter.toLowerCase())));
+        result = table.filter(obj => Object.keys(obj).some(key => obj[key].toLowerCase().includes(searchFilter.toLowerCase())));
         resultSet = result.map(m => m.shortname);
     }
 
     return (
-        <><div className="block_filter">Type to search: <input value={searchFilter} onChange={handleChange} /></div>
+        <><div className="block_filter">Click to filter: {profiles.map((props,idx) => <FilterButton key={idx} {...props} />)}<br />
+        Type to search: <input value={searchFilter} onChange={handleChange} /></div>
         <div><MethodsTable methods_to_show={resultSet} /></div></>    
     )
 }
