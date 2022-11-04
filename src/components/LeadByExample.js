4f4564bbf65661329daf6e54631cf39863664dd0
Johannes Liermann <liermann@uni-mainz.de>
2022-11-04 19:14:18 +0100

    Update LeadByExample.js

--- src/components/LeadByExample.js
+++ src/components/LeadByExample.js
@@ -1,4 +1,4 @@
-import React,{ useState, UseRef } from "react";
+import React,{ useState } from "react";
 import { useLocation } from "react-router-dom"
 
 var lbeTable = require('@site/static/assets/lbe.json');
