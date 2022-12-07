5badc21389cc045261289de9939015c3b004071e
Johannes Liermann <liermann@uni-mainz.de>
2022-12-07 23:25:43 +0100

    chore: remove useRef

--- src/components/LeadByExample.js
+++ src/components/LeadByExample.js
@@ -1,4 +1,4 @@
-import React,{ useState, useRef, useEffect } from "react";
+import React,{ useState, useEffect } from "react";
 import { useLocation } from "react-router-dom"
 
 var lbeTable = require('@site/static/assets/lbe.json');
