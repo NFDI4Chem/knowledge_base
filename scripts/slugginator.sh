a9c126be8b8fb81da220b3aaec7f3eace38e4e9d
Johannes Liermann <liermann@uni-mainz.de>
2022-12-21 16:36:20 +0100

    chore: comment script

--- scripts/slugginator.sh
+++ scripts/slugginator.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-## Script to automatically add slug to frontmatter for permalinking
+## Bash script to automatically add slug to frontmatter for permalinking
+## Usage example: slugginator.sh *.mdx to "slug" all mdx files in present directory
+
+# Regex for matching file name without number and ending
 
 regex='[0-9]+\_([a-zA-Z0-9_-]*).mdx'
 
 for i in "$@"
 do
     echo "$i"
+
+    # check if filename fits the pattern
+
     if [[ $i =~ $regex ]]; then
+
+        #compose slug string from filename
         slug="slug: \"/docs/${BASH_REMATCH[1]}\""
-        echo $slug
-        sed -i '/slug:.*/d' $i                                          # Remove existing slug
+
+        echo "File $i: Slug is added to frontmatter:\n$slug"
+        
+        # Remove existing slug
+        sed -i '/slug:.*/d' $i
         sed -i ':a;N;$!ba; s,---,'"$slug"'\n---,2' $i
     fi
  done
