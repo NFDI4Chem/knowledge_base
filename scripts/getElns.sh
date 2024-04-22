70240b329798479a401cf95ab1096548aa018f07
Johannes Liermann <liermann@uni-mainz.de>
2024-04-22 13:03:12 +0200

    chore: target dir for eln script

--- scripts/getElns.sh
+++ scripts/getElns.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+targetDir="/git/n4c-kb_jl/build/assets"
+
 # get the data from the eln-finder
 
 curl -o eln-finder.json "https://eln-finder.ulb.tu-darmstadt.de/server/api/discover/search/objects?sort=dc.title,ASC&f.K03=Chemistry,equals"
 
 # add current date and time to the json
 
-jq ". + {date: \"$(date -u)\" }" eln-finder.json > eln-finder-dated.json
+jq ". + {date: \"$(date -u)\" }" eln-finder.json > $targetDir/eln-finder-dated.json
