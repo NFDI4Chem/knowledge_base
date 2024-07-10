8be570c8559387ee5486f43ffee4cdd0170eb9bb
Johannes Liermann <liermann@uni-mainz.de>
2024-07-10 17:31:03 +0200

    feat: improve behaviour and styling

--- src/components/eln/elnFilter/TextSearch.js
+++ src/components/eln/elnFilter/TextSearch.js
@@ -1,19 +1,25 @@
+import styles from "../Eln.module.css";
+
 function TextSearch({ resultOutput, filter, setFilter }) {
     const handleChange = (e) =>
         setFilter((draft) => {
             draft.text = e.target.value;
         });
 
+    console.log(filter);
+
     return (
-        <div className="lbe__searchfilter__search">
-            <input
-                type="search"
-                className="navbar__search-input"
-                placeholder="Type to search"
-                value={filter.text}
-                onChange={handleChange}
-            />
-            &ensp;
+        <div className={styles.eln__searchfilter__search}>
+            <span>
+                <input
+                    type="search"
+                    className="navbar__search-input"
+                    placeholder="Type to search"
+                    value={filter.text ? filter.text : ""}
+                    onChange={handleChange}
+                />
+                &nbsp;
+            </span>
             <em>{resultOutput}</em>
         </div>
     );
