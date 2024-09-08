71f0d76b8b8997d8dcf678fdc478445e73c6034d
Johannes Liermann <liermann@uni-mainz.de>
2024-09-08 13:33:27 +0200

    feat: get pharmacy data

--- scripts/getElns.sh
+++ scripts/getElns.sh
@@ -7,6 +7,9 @@ targetDir="/var/www/html/staging_eln/knowledge_base/assets"
 
 curl -o eln-finder.json "https://eln-finder.ulb.tu-darmstadt.de/server/api/discover/search/objects?sort=dc.title,ASC&f.K03=Chemistry,equals"
 
+curl -o eln-finder-pharm.json "https://eln-finder.ulb.tu-darmstadt.de/server/api/discover/search/objects?sort=dc.title,ASC&f.K03=Pharmacy,equals"
+
 # add current date and time to the json
 
 jq ". + {date: \"$(date -u)\" }" eln-finder.json > $targetDir/elnData.json
+jq ". + {date: \"$(date -u)\" }" eln-finder-pharm.json > $targetDir/elnDataPharm.json
