2d4f40305405babc27ecbd6d100efff032e89af1
Johannes Liermann <liermann@uni-mainz.de>
2025-12-11 16:58:24 +0100

    feat: make image width and margin configurable in ChemotionIcon component

--- src/components/chemotion/ChemotionIcon.js
+++ src/components/chemotion/ChemotionIcon.js
@@ -5,11 +5,10 @@ function ChemotionIcon({url, alt, ...props}) {
 		<img
 			alt={alt}
 			src={useBaseUrl(url)}
-			width="200"
 			style={{
-				width: "min(120px, 50%)",
+				width: props.width ?? "min(120px, 50%)",
 				float: props.float ?? "right",
-				margin: "0px 0px 0px 40px",
+				margin: props.margin ?? "0px 20px 0px 20px",
 			}}
 		/>
 	);
