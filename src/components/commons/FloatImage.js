e806697a445ef2e0e90b6ac683358634169ddd10
Johannes Liermann <liermann@uni-mainz.de>
2026-01-08 11:55:52 +0100

    chore: prettier styling

--- src/components/commons/FloatImage.js
+++ src/components/commons/FloatImage.js
@@ -1,16 +1,16 @@
 import useBaseUrl from "@docusaurus/useBaseUrl";
 
-function FloatImage({url, alt, ...props}) {
-	return (
-		<img
-			alt={alt}
-			src={useBaseUrl(url)}
-			style={{
-				width: props.width ?? "min(120px, 50%)",
-				float: props.float ?? "right",
-				margin: props.margin ?? "0px 20px 0px 20px",
-			}}
-		/>
-	);
+function FloatImage({ url, alt, ...props }) {
+  return (
+    <img
+      alt={alt}
+      src={useBaseUrl(url)}
+      style={{
+        width: props.width ?? "min(120px, 50%)",
+        float: props.float ?? "right",
+        margin: props.margin ?? "0px 20px 0px 20px",
+      }}
+    />
+  );
 }
 export default FloatImage;
