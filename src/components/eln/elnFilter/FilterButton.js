735fa6318801160a82ffdd324c6a1ed61d779c51
Johannes Liermann <liermann@uni-mainz.de>
2026-01-12 13:23:27 +0100

    chore: styles

--- src/components/eln/elnFilter/FilterButton.js
+++ src/components/eln/elnFilter/FilterButton.js
@@ -3,30 +3,30 @@ import clsx from "clsx";
 import styles from "../Eln.module.css";
 
 function FilterButton(props) {
-    const handleClick = () => {
-        if (props.label === "All") {
-            props.setFilter((draft) => {
-                delete draft[props.type];
-            });
-        } else {
-            props.setFilter((draft) => {
-                draft[props.type] = props.label;
-            });
-        }
-    };
+  const handleClick = () => {
+    if (props.label === "All") {
+      props.setFilter((draft) => {
+        delete draft[props.type];
+      });
+    } else {
+      props.setFilter((draft) => {
+        draft[props.type] = props.label;
+      });
+    }
+  };
 
-    // Conditional styling for button
+  // Conditional styling for button
 
-    let buttonClass = clsx("lbe__filterbutton", {
-        [styles["eln__filterbutton--secondary"]]: props.secondary,
-        "lbe__filterbutton--active": props.active,
-    });
+  let buttonClass = clsx("lbe__filterbutton", {
+    [styles["eln__filterbutton--secondary"]]: props.secondary,
+    "lbe__filterbutton--active": props.active,
+  });
 
-    return (
-        <button className={buttonClass} onClick={handleClick}>
-            {props.label}
-        </button>
-    );
+  return (
+    <button className={buttonClass} onClick={handleClick}>
+      {props.label}
+    </button>
+  );
 }
 
 export default FilterButton;
