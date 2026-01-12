b3a6de3f5cd51e161d214124c0156c26dd6cec88
Johannes Liermann <liermann@uni-mainz.de>
2026-01-12 16:00:11 +0100

    chore: formatting

--- src/components/repos/RepoButton.js
+++ src/components/repos/RepoButton.js
@@ -4,16 +4,16 @@ import Link from "@docusaurus/Link";
 import IconExternalLink from "@theme/Icon/ExternalLink";
 
 function RepoButton(props) {
-    return (
-        <React.Fragment>
-            <Link to={props.url} target="_blank">
-                <button className="lbe__filterbutton lbe__chip">
-                    {props.intro ? props.intro + " " : null}
-                    <strong>{props.name}</strong> <IconExternalLink />
-                </button>
-            </Link>
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      <Link to={props.url} target="_blank">
+        <button className="lbe__filterbutton lbe__chip">
+          {props.intro ? props.intro + " " : null}
+          <strong>{props.name}</strong> <IconExternalLink />
+        </button>
+      </Link>
+    </React.Fragment>
+  );
 }
 
 export default RepoButton;
