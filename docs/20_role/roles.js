456542526a33a9cb4d63695cf7a10017852df272
Johannes Liermann <liermann@uni-mainz.de>
2026-01-15 17:29:08 +0100

    refactor: replace N4CFeatures with Features component and add roles data

---
+++ docs/20_role/roles.js
@@ -0,0 +1,31 @@
+import Translate from "@docusaurus/Translate";
+
+const roles = [
+  {
+    text: <Translate>Research Group Leader</Translate>,
+    imgUrl: "/img/nfdi4chem_Research_Group_Leader.svg",
+    url: "/docs/research_group_leader",
+  },
+  {
+    text: <Translate>Research Group Member</Translate>,
+    imgUrl: "/img/nfdi4chem_Research_Group_Member.svg",
+    url: "/docs/research_group_member",
+  },
+  {
+    text: <Translate>Student</Translate>,
+    imgUrl: "/img/nfdi4chem_Student.svg",
+    url: "/docs/student",
+  },
+  {
+    text: <Translate>Data Steward</Translate>,
+    imgUrl: "/img/nfdi4chem_Data_Steward.svg",
+    url: "/docs/data_steward",
+  },
+  {
+    text: <Translate>Core Facility Manager</Translate>,
+    imgUrl: "/img/nfdi4chem_Core_Facility_Manager.svg",
+    url: "/docs/core_facility_manager",
+  },
+];
+
+export default roles;
