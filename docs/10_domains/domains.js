38b32750f1904e5504409ed751bc78419a3a2a21
Johannes Liermann <liermann@uni-mainz.de>
2026-01-15 17:12:56 +0100

    refactor: replace N4CFeatures with Features component and add domains data

---
+++ docs/10_domains/domains.js
@@ -0,0 +1,31 @@
+import Translate from "@docusaurus/Translate";
+
+const domains = [
+  {
+    text: <Translate>Analytical Chemistry</Translate>,
+    imgUrl: "/img/nfdi4chem_Analytical_Chemistry.svg",
+    url: "/docs/analytical_chemistry",
+  },
+  {
+    text: <Translate>Electrochemistry</Translate>,
+    imgUrl: "/img/nfdi4chem_Electrochemistry.svg",
+    url: "/docs/electrochemistry",
+  },
+  {
+    text: <Translate>Pharmaceutical Chemistry</Translate>,
+    imgUrl: "/img/nfdi4chem_Medicinal-Pharmaceutical_Chemistry.svg",
+    url: "/docs/pharmaceutical_chemistry",
+  },
+  {
+    text: <Translate>Physical Chemistry</Translate>,
+    imgUrl: "/img/nfdi4chem_Physial_Chemistry.svg",
+    url: "/docs/physical_chemistry",
+  },
+  {
+    text: <Translate>Synthetic Chemistry</Translate>,
+    imgUrl: "/img/nfdi4chem_Synthetic_Chemistry.svg",
+    url: "/docs/synthetic_chemistry",
+  },
+];
+
+export default domains;
