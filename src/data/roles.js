dfd730a54addbaf36ab1cdf330e907636f6eac1d
Johannes Liermann <liermann@uni-mainz.de>
2026-01-26 16:58:41 +0100

    fix: add alt attributes for improved accessibility in domains, roles, stakeholders, and index components

--- src/data/roles.js
+++ src/data/roles.js
@@ -4,26 +4,31 @@ const roles = [
   {
     text: <Translate>Research Group Leader</Translate>,
     imgUrl: "/img/nfdi4chem_Research_Group_Leader.svg",
+    alt: "Research Group Leader Icon",
     url: "/docs/research_group_leader",
   },
   {
     text: <Translate>Research Group Member</Translate>,
     imgUrl: "/img/nfdi4chem_Research_Group_Member.svg",
+    alt: "Research Group Member Icon",
     url: "/docs/research_group_member",
   },
   {
     text: <Translate>Student</Translate>,
     imgUrl: "/img/nfdi4chem_Student.svg",
+    alt: "Student Icon",
     url: "/docs/student",
   },
   {
     text: <Translate>Data Steward</Translate>,
     imgUrl: "/img/nfdi4chem_Data_Steward.svg",
+    alt: "Data Steward Icon",
     url: "/docs/data_steward",
   },
   {
     text: <Translate>Core Facility Manager</Translate>,
     imgUrl: "/img/nfdi4chem_Core_Facility_Manager.svg",
+    alt: "Core Facility Manager Icon",
     url: "/docs/core_facility_manager",
   },
 ];
