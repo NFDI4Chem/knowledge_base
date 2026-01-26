7ea64d19fe43f641682708126616927ffe279099
Johannes Liermann <liermann@uni-mainz.de>
2026-01-26 16:29:34 +0100

    chore: adapt copyright message and improve styling

--- copyright.js
+++ copyright.js
@@ -1,13 +1,13 @@
 const html = `
-    <div class="footer__copyright row" style="justify-content: center">
-        <div class="cell" style="padding: 1rem;" >
-            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" style="border-width:0" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>
+        <div class="footer__copyright--image">
+            <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="Creative Commons License" src="https://i.creativecommons.org/l/by-sa/4.0/88x31.png"></a>
         </div>
-        <div class="cell" style="padding: 0.1rem; text-align: left;" >
-            Licensed under a <a class="footer__link-item" rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA 4.0</a> License, if not stated otherwise.<br />
-            Copyright © ${new Date().getFullYear()} NFDI4Chem. Built with Docusaurus.
+        <div class="footer__copyright--text">
+            <p>
+                Licensed under a <a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">Creative Commons BY-SA 4.0</a> License, if not stated otherwise. IBM Plex Sans is licensed under the <a rel="license" href="https://openfontlicense.org/open-font-license-official-text/">SIL Open Font License, Version 1.1</a>.<br />
+                Copyright © ${new Date().getFullYear()} NFDI4Chem. Built with Docusaurus.
+            </p>
         </div>
-    </div>
 `;
 
 module.exports = html;
