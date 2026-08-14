1535cf74ad80c9928a37f0e4218cfc7be23ec95f
Johannes Liermann <liermann@uni-mainz.de>
2026-08-14 13:26:58 +0200

    refactor: import style and content from static js to keep mdx clean

---
+++ docs/50_data_publication/20_choose_repository/repoCardData.js
@@ -0,0 +1,54 @@
+export const repositoryData = [
+    {
+        name: "Chemotion Repository",
+        url: "/img/ChemotionRepo_Logo.svg",
+        alt: "Chemotion Repository Logo",
+        description:
+            "Field-specific sample and reaction-centric repository including analysis data such as NMR, UV-VIS, IR, and MS data.",
+    },
+    {
+        name: "MassBank",
+        url: "/img/Massbank_logo.svg",
+        alt: "Massbank Logo",
+        description:
+            "Field-specific ecosystem of databases and tools for mass spectrometry reference spectra.",
+        note: "*",
+    },
+    {
+        name: "nmrXiv",
+        url: "/img/nmrXiv.svg",
+        alt: "nmrXiv Logo",
+        description: "Field-specific repository for NMR data.",
+    },
+    {
+        name: "RADAR4Chem",
+        url: "/img/radar4chem_Logo.svg",
+        alt: "RADAR4Chem Logo",
+        description:
+            "Generic, multidisciplinary repository that offers a free and reliable home for all chemical research data that do not fulfil the specifications of field-specific repositories.",
+    },
+    {
+        name: "STRENDA",
+        url: "/img/Logo_Beilstein_STRENDA_sRGB.jpg",
+        alt: "Strenda DB Logo",
+        description:
+            "Field-specific repository for enzymology data, which incorporates the STRENDA Guidelines for reporting enzymology data.",
+    },
+    {
+        name: "Suprabank",
+        url: "/img/Suprabank_logo.svg",
+        alt: "Suprabank Logo",
+        description:
+            "Field-specific repository for intermolecular interactions data.",
+    },
+];
+
+export const repositoryStyle = {
+    "--ifm-button-size-multiplier": "1",
+    flexBasis: "250px",
+    fontWeight: "unset",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "space-evenly",
+};
