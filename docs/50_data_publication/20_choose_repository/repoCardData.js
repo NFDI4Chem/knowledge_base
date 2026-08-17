c6ea88fb5e73da0da9293d5c2209153a26fef09f
Johannes Liermann <liermann@uni-mainz.de>
2026-08-17 18:28:52 +0200

    chore: reorganize logo locations

--- docs/50_data_publication/20_choose_repository/repoCardData.js
+++ docs/50_data_publication/20_choose_repository/repoCardData.js
@@ -1,42 +1,41 @@
 export const repositoryData = [
     {
         name: "Chemotion Repository",
-        url: "/img/ChemotionRepo_Logo.svg",
+        url: "/img/data_pub/repos/ChemotionRepo_Logo.svg",
         alt: "Chemotion Repository Logo",
         description:
             "Field-specific sample and reaction-centric repository including analysis data such as NMR, UV-VIS, IR, and MS data.",
     },
     {
         name: "MassBank",
-        url: "/img/Massbank_logo.svg",
+        url: "/img/data_pub/repos/Massbank_logo.svg",
         alt: "Massbank Logo",
         description:
-            "Field-specific ecosystem of databases and tools for mass spectrometry reference spectra.",
-        note: "*",
+            "Field-specific ecosystem of databases and tools for mass spectrometry reference spectra.*",
     },
     {
         name: "nmrXiv",
-        url: "/img/nmrXiv.svg",
+        url: "/img/data_pub/repos/nmrXiv.svg",
         alt: "nmrXiv Logo",
         description: "Field-specific repository for NMR data.",
     },
     {
         name: "RADAR4Chem",
-        url: "/img/radar4chem_Logo.svg",
+        url: "/img/data_pub/repos/radar4chem_Logo.svg",
         alt: "RADAR4Chem Logo",
         description:
             "Generic, multidisciplinary repository that offers a free and reliable home for all chemical research data that do not fulfil the specifications of field-specific repositories.",
     },
     {
         name: "STRENDA",
-        url: "/img/Logo_Beilstein_STRENDA_sRGB.jpg",
+        url: "/img/data_pub/repos/Logo_Beilstein_STRENDA_sRGB.jpg",
         alt: "Strenda DB Logo",
         description:
             "Field-specific repository for enzymology data, which incorporates the STRENDA Guidelines for reporting enzymology data.",
     },
     {
         name: "Suprabank",
-        url: "/img/Suprabank_logo.svg",
+        url: "/img/data_pub/repos/Suprabank_logo.svg",
         alt: "Suprabank Logo",
         description:
             "Field-specific repository for intermolecular interactions data.",
