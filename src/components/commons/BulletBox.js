26a49bbeac7b0957a7589cb97de8ea51a812f3b0
Johannes Liermann <liermann@uni-mainz.de>
2026-09-04 18:05:27 +0200

    feat: add link support to BulletBox component

--- src/components/commons/BulletBox.js
+++ src/components/commons/BulletBox.js
@@ -1,4 +1,5 @@
 import clsx from "clsx";
+import Link from "@docusaurus/Link";
 import styles from "@site/src/css/BulletBox.module.css";
 
 function BulletContainer({ children }) {
@@ -8,26 +9,25 @@ function BulletContainer({ children }) {
 function BulletBox({ children, secondary, ...props }) {
 	let boxClass = secondary ? "button--secondary" : "button--primary";
 	let customStyle = {};
+	let link = props.link ?? null;
 
 	Object.keys(props).forEach((key) => {
-		if (key !== "children" && key !== "secondary" && key !== "boxClass") {
+		if (key !== "children" && key !== "secondary" && key !== "boxClass" && key !== "link") {
 			customStyle[key] = props[key];
 		}
 	});
 
-	return (
-		<div
-			className={clsx("col", "button", "button--lg", boxClass)}
-			style={{
-				padding: "0.75em",
-				margin: "0.4em",
-				flexGrow: 1,
-				...customStyle,
-			}}
-		>
-			{children}
-		</div>
-	);
+	return (<div
+		className={clsx("col", "button", "button--lg", boxClass)}
+		style={{
+			padding: "0.75em",
+			margin: "0.4em",
+			flexGrow: 1,
+			...customStyle,
+		}}
+	>
+		{link ? <Link to={link} className={styles.bulletBox_Link}>{children}</Link> : { children }}
+	</div>)
 }
 
 export { BulletContainer, BulletBox };
