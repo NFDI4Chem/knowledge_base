a687dd40fa025d5ac6641cd75ca6c9eba694719d
Johannes Liermann <liermann@uni-mainz.de>
2026-09-04 18:39:37 +0200

    refactor: implement translatable content for feature buttons

---
+++ src/data/dataFeatures.js
@@ -0,0 +1,37 @@
+import Translate from "@docusaurus/Translate";
+
+const dataFeatures = {
+    dmp: {
+        url: "/docs/dmp",
+        imgUrl: "/img/nfdi4chem_Data_Management_Plan.svg",
+        text: <Translate>Data Management Plan</Translate>
+    },
+
+    dataOrganisation: {
+        url: "/docs/data_organisation",
+        imgUrl: "/img/nfdi4chem_Data_Organisation.svg",
+        text: <Translate>Data Organisation</Translate>
+    },
+    dataDocumentation: {
+        url: "/docs/data_documentation",
+        imgUrl: "/img/nfdi4chem_Data_Documentation.svg",
+        text: <Translate>Data Documentation</Translate>
+    },
+    dataStorage: {
+        url: "/docs/data_storage",
+        imgUrl: "/img/nfdi4chem_Data_Storage_and_Archiving.svg",
+        text: <Translate>Data Storage and Archiving</Translate>
+    },
+    dataPublishing: {
+        url: "/docs/data_publishing",
+        imgUrl: "/img/nfdi4chem_Data_Publication.svg",
+        text: <Translate>Data Publishing</Translate>
+    },
+    chooseRepository: {
+        url: "/docs/choose_repository",
+        imgUrl: "/img/nfdi4chem_How_to_choose_the_right_repository.svg",
+        text: <Translate>How to choose the right repository</Translate>
+    }
+}
+
+export default dataFeatures;
