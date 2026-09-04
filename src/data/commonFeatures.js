8236a9d84b5ac91e58d3522e469b13951aeec349
Johannes Liermann <liermann@uni-mainz.de>
2026-09-04 19:12:34 +0200

    chore: styling

--- src/data/commonFeatures.js
+++ src/data/commonFeatures.js
@@ -1,71 +1,71 @@
 import Translate from "@docusaurus/Translate";
 
 const commonFeatures = {
-    domains: {
-        url: "/docs/domain_guide",
-        imgUrl: "/img/nfdi4chem_Domains.svg",
-        text: <Translate>Domains</Translate>
-    },
-    roles: {
-        url: "/docs/role_guide/",
-        imgUrl: "/img/nfdi4chem_Roles.svg",
-        text: <Translate>Roles</Translate>
-    },
-    handlingData: {
-        url: "/docs/data_guide/",
-        imgUrl: "/img/nfdi4chem_Handling_Data.svg",
-        text: <Translate>Handling Data</Translate>
-    },
-    smartlab: {
-        url: "/docs/smartlab",
-        imgUrl: "/img/nfdi4chem_SmartLab.svg",
-        text: <Translate>Smartlab</Translate>
-    },
-    eln: {
-        url: "/docs/eln",
-        imgUrl: "/img/nfdi4chem_SmartLab.svg",
-        text: <Translate>Electronic Lab Notebooks</Translate>
-    },
-    dataPublishing: {
-        url: "/docs/data_publishing",
-        imgUrl: "/img/nfdi4chem_Data_Publication.svg",
-        text: <Translate>Data Publishing</Translate>
-    },
-    dmp: {
-        url: "/docs/dmp",
-        imgUrl: "/img/nfdi4chem_Data_Management_Plan.svg",
-        text: <Translate>Data Management Plan</Translate>
-    },
-    dataOrganisation: {
-        url: "/docs/data_organisation",
-        imgUrl: "/img/nfdi4chem_Data_Organisation.svg",
-        text: <Translate>Data Organisation</Translate>
-    },
-    dataDocumentation: {
-        url: "/docs/data_documentation",
-        imgUrl: "/img/nfdi4chem_Data_Documentation.svg",
-        text: <Translate>Data Documentation</Translate>
-    },
-    dataStorage: {
-        url: "/docs/data_storage",
-        imgUrl: "/img/nfdi4chem_Data_Storage_and_Archiving.svg",
-        text: <Translate>Data Storage and Archiving</Translate>
-    },
-    dataPublishing: {
-        url: "/docs/data_publishing",
-        imgUrl: "/img/nfdi4chem_Data_Publication.svg",
-        text: <Translate>Data Publishing</Translate>
-    },
-    chooseRepository: {
-        url: "/docs/choose_repository",
-        imgUrl: "/img/nfdi4chem_How_to_choose_the_right_repository.svg",
-        text: <Translate>How to choose the right repository</Translate>
-    },
-    lbe: {
-        url: "/docs/datasets",
-        imgUrl: "/img/nfdi4chem_Lead_by_Example.svg",
-        text: <Translate>View Datasets</Translate>
-    }
-}
+	domains: {
+		url: "/docs/domain_guide",
+		imgUrl: "/img/nfdi4chem_Domains.svg",
+		text: <Translate>Domains</Translate>,
+	},
+	roles: {
+		url: "/docs/role_guide/",
+		imgUrl: "/img/nfdi4chem_Roles.svg",
+		text: <Translate>Roles</Translate>,
+	},
+	handlingData: {
+		url: "/docs/data_guide/",
+		imgUrl: "/img/nfdi4chem_Handling_Data.svg",
+		text: <Translate>Handling Data</Translate>,
+	},
+	smartlab: {
+		url: "/docs/smartlab",
+		imgUrl: "/img/nfdi4chem_SmartLab.svg",
+		text: <Translate>Smartlab</Translate>,
+	},
+	eln: {
+		url: "/docs/eln",
+		imgUrl: "/img/nfdi4chem_SmartLab.svg",
+		text: <Translate>Electronic Lab Notebooks</Translate>,
+	},
+	dataPublishing: {
+		url: "/docs/data_publishing",
+		imgUrl: "/img/nfdi4chem_Data_Publication.svg",
+		text: <Translate>Data Publishing</Translate>,
+	},
+	dmp: {
+		url: "/docs/dmp",
+		imgUrl: "/img/nfdi4chem_Data_Management_Plan.svg",
+		text: <Translate>Data Management Plan</Translate>,
+	},
+	dataOrganisation: {
+		url: "/docs/data_organisation",
+		imgUrl: "/img/nfdi4chem_Data_Organisation.svg",
+		text: <Translate>Data Organisation</Translate>,
+	},
+	dataDocumentation: {
+		url: "/docs/data_documentation",
+		imgUrl: "/img/nfdi4chem_Data_Documentation.svg",
+		text: <Translate>Data Documentation</Translate>,
+	},
+	dataStorage: {
+		url: "/docs/data_storage",
+		imgUrl: "/img/nfdi4chem_Data_Storage_and_Archiving.svg",
+		text: <Translate>Data Storage and Archiving</Translate>,
+	},
+	dataPublishing: {
+		url: "/docs/data_publishing",
+		imgUrl: "/img/nfdi4chem_Data_Publication.svg",
+		text: <Translate>Data Publishing</Translate>,
+	},
+	chooseRepository: {
+		url: "/docs/choose_repository",
+		imgUrl: "/img/nfdi4chem_How_to_choose_the_right_repository.svg",
+		text: <Translate>How to choose the right repository</Translate>,
+	},
+	lbe: {
+		url: "/docs/datasets",
+		imgUrl: "/img/nfdi4chem_Lead_by_Example.svg",
+		text: <Translate>View Datasets</Translate>,
+	},
+};
 
 export default commonFeatures;
