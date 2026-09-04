f81085f13e03f6383be3b72140d54603784cefb8
Johannes Liermann <liermann@uni-mainz.de>
2026-09-04 18:47:21 +0200

    refactor: consolidate feature button data into commonFeatures for improved localization

--- src/data/commonFeatures.js
+++ src/data/commonFeatures.js
@@ -1,12 +1,41 @@
 import Translate from "@docusaurus/Translate";
 
-const dataFeatures = {
+const commonFeatures = {
+    domains: {
+        url: "/docs/domain_guide",
+        imgUrl: "/img/nfdi4chem_Domains.svg",
+        text: <Translate>Domains</Translate>
+    },
+    roles: {
+        url: "/docs/role_guide/",
+        imgUrl: "/img/nfdi4chem_Roles.svg",
+        text: <Translate>Roles</Translate>
+    },
+    handlingData: {
+        url: "/docs/data_guide/",
+        imgUrl: "/img/nfdi4chem_Handling_Data.svg",
+        text: <Translate>Handling Data</Translate>
+    },
+    smartlab: {
+        url: "/docs/smartlab",
+        imgUrl: "/img/nfdi4chem_SmartLab.svg",
+        text: <Translate>Smartlab</Translate>
+    },
+    eln: {
+        url: "/docs/eln",
+        imgUrl: "/img/nfdi4chem_SmartLab.svg",
+        text: <Translate>Electronic Lab Notebook</Translate>
+    },
+    dataPublishing: {
+        url: "/docs/data_publishing",
+        imgUrl: "/img/nfdi4chem_Data_Publication.svg",
+        text: <Translate>Data Publishing</Translate>
+    },
     dmp: {
         url: "/docs/dmp",
         imgUrl: "/img/nfdi4chem_Data_Management_Plan.svg",
         text: <Translate>Data Management Plan</Translate>
     },
-
     dataOrganisation: {
         url: "/docs/data_organisation",
         imgUrl: "/img/nfdi4chem_Data_Organisation.svg",
@@ -31,7 +60,12 @@ const dataFeatures = {
         url: "/docs/choose_repository",
         imgUrl: "/img/nfdi4chem_How_to_choose_the_right_repository.svg",
         text: <Translate>How to choose the right repository</Translate>
+    },
+    lbe: {
+        url: "/docs/datasets",
+        imgUrl: "/img/nfdi4chem_Lead_by_Example.svg",
+        text: <Translate>View Datasets</Translate>
     }
 }
 
-export default dataFeatures;
+export default commonFeatures;
