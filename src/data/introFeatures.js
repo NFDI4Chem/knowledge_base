8c5735d062fb6ef53b8dce3cd1846f3aebcc23fa
Johannes Liermann <liermann@uni-mainz.de>
2026-09-04 18:34:25 +0200

    refactor: remove button data for better localisation

---
+++ src/data/introFeatures.js
@@ -0,0 +1,31 @@
+import Translate from "@docusaurus/Translate";
+
+const introFeatures = {
+    domains: {
+        url: "/docs/domain_guide",
+        imgUrl: "/img/nfdi4chem_Domains.svg",
+        text: <Translate>Domains</Translate>
+    },
+    roles: {
+        url: "/docs/role_guide/",
+        imgUrl: "/img/nfdi4chem_Roles.svg",
+        text: <Translate>Roles</Translate>
+    },
+    handlingData: {
+        url: "/docs/data_guide/",
+        imgUrl: "/img/nfdi4chem_Handling_Data.svg",
+        text: <Translate>Handling Data</Translate>
+    },
+    smartlab: {
+        url: "/docs/smartlab",
+        imgUrl: "/img/nfdi4chem_SmartLab.svg",
+        text: <Translate>Smartlab</Translate>
+    },
+    dataPublishing: {
+        url: "/docs/data_publishing",
+        imgUrl: "/img/nfdi4chem_Data_Publication.svg",
+        text: <Translate>Data Publishing</Translate>
+    }
+}
+
+export default introFeatures;
